3c3b18f7bb70d379dfed5968b701fc4c6fcc32c3
Patrick Callaghan <patcho@gmail.com>
2015-01-02 15:30:37 +0000

    updated to full example full MySQL copy.

--- src/main/scala/SparkCopyMySQL.scala
+++ src/main/scala/SparkCopyMySQL.scala
@@ -10,6 +10,8 @@ import java.sql.{ Connection, DriverManager, ResultSet }
 object SparkCopyMySQL {
 
   case class Account(user_id: String, account_id: String, balance: Double)
+  
+  case class User(id: java.util.UUID, first: String, last: String, email: String, country: String, phone: String);
 
   def main(args: Array[String]) {
     val sparkConf = new SparkConf(true)
@@ -17,18 +19,33 @@ object SparkCopyMySQL {
 
     val sc = new SparkContext("spark://127.0.0.1:7077", "SparkSync", sparkConf)
 
-    val url = "jdbc:mysql://192.168.25.172:3306/testdb"
+    val url = "jdbc:mysql://localhost:3306/testdb"
     val user = "root"
-    val password = "elephant"
+    val password = ""
 
     Class.forName("com.mysql.jdbc.Driver").newInstance
 
-    val myRDD = new JdbcRDD(sc, () => DriverManager.getConnection(url, user, password), 
-        "select * from accounts limit ?, ?", 0, 1000000, 10, 
+    val accountsRDD = new JdbcRDD(sc, () => DriverManager.getConnection(url, user, password), 
+        "select * from accounts limit ?, ?", 0, 10000, 3, 
         r => new Account(r.getString("user_id"), r.getString("account_id"), r.getDouble("balance")))
 
-    println("Processing Table")
-    myRDD.collect.foreach(println)
+    println("Processing Account Table")
+    accountsRDD.partitions.foreach(println)
+    //accountsRDD.collect.foreach(println)
+    println(accountsRDD.collect.toList.size)
+
+    val usersRDD = new JdbcRDD(sc, () => DriverManager.getConnection(url, user, password), 
+        "select * from users limit ?, ?", 0, 100000000, 10, 
+        r => new User(java.util.UUID.fromString(r.getString("id")), r.getString("first"), r.getString("last"), r.getString("email"), r.getString("country"), r.getString("phone")))
+
+    println("Processing User Table")
+    usersRDD.partitions.foreach(println)
+    //usersRDD.collect.foreach(println)
+    println(usersRDD.collect.toList.size)
+    
+    println("Copying User Table")
+    usersRDD.saveToCassandra("test", "users")
+    println("Copied User Table to Cassandra")
   }
 }
 
@@ -39,4 +56,4 @@ object SparkCopyMySQL {
 
 
 
-
+	
